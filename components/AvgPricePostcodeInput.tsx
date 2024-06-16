cc08c057f7b149d49e6df1dbe1e5f75a9c5130a0
Jagadeesh Posni <jagadeeshposni@gmail.com>
2024-06-16 11:39:36 +0100

    Fix some bugs on setting error

--- components/AvgPricePostcodeInput.tsx
+++ components/AvgPricePostcodeInput.tsx
@@ -7,91 +7,99 @@ import { useDebouncedCallback } from 'use-debounce';
 
 export function AvgPricePostcodeInput() {
     const [ac, setAc] = useState([]);
-    const [postcodeError, setPostcodeError] = useState<boolean | string>(false);
-    const [postcodeValue, setPostcodeValue] = useState('');
+    const [postcodeError, setPostcodeError] = useState<boolean>(false);
+    const [postcodeInput, setPostcodeInput] = useState('');
 
     const searchParams = useSearchParams();
     const pathname = usePathname();
     const { replace } = useRouter();
 
-    const handleSearch = (term: string) => {
+    const handleSearch = async (term: string) => {
+        console.debug('searching for postcode' + term);
         setPostcodeError(false);
-        if (term === undefined || term === null || term === '') {
-            const searchInput = document.getElementById('search') as HTMLInputElement | null;
-            term = searchInput?.value ?? '';
-        }
         const params = new URLSearchParams(searchParams);
-        //validaate postcode using postcodes.io api to look for outcode and if not found set autocomplete field to error
-        //validate postcode using postcodes.io api and set autocomplete field to error if invalid
-        fetch(`https://api.postcodes.io/postcodes/${term}/validate`)
+        const response = await fetch(`https://api.postcodes.io/postcodes/${term}/validate`);
+        const data = await response.json();
+        if (data.result === false) {
+            const outcodeResponse = await fetch(`https://api.postcodes.io/outcodes/${term}`);
+            const outcodeData = await outcodeResponse.json();
+            if (outcodeData.status === 404) {
+                console.debug('setting error');
+                setPostcodeError(true);
+                console.debug('error set');
+            } else {
+                console.debug('setting query');
+                if (!postcodeError) {
+                    if (term) {
+                        params.set('query', term);
+                    } else {
+                        params.delete('query');
+                    }
+                    replace(`${pathname}?${params.toString()}`);
+                }
+            }
+        }else{
+            if (!postcodeError) {
+                if (term) {
+                    params.set('query', term);
+                } else {
+                    params.delete('query');
+                }
+                replace(`${pathname}?${params.toString()}`);
+            }
+        }
+    }
+
+    const fetchPostCodesData = useDebouncedCallback((term) => {
+        fetch(`https://api.postcodes.io/postcodes/${term}/autocomplete`)
             .then((response) => response.json())
             .then((data) => {
-                if (data.result === false) {
-                    fetch(`https://api.postcodes.io/outcodes/${term}`)
-                        .then((response) => response.json())
-                        .then((data) => {
-                            if (data.status === 404) {
-                                setPostcodeError("Invalid postcode");
-                            }
-                        });
-                }
+                setAc(data.result);
             });
-        if (postcodeError !== "Invalid postcode") {
-            if (term) {
-                params.set('query', term);
-            } else {
-                params.delete('query');
-            }
-            replace(`${pathname}?${params.toString()}`);
-        }
+    }, 500);
+   
+    const handleChange = (term: string) => {
+        setPostcodeInput(term);
+        setPostcodeError(false);
+        fetchPostCodesData(term);
     }
 
+    return (
+        <Flex
+            mih={50}
+            gap="xl"
+            justify="center"
+            align="flex-end"
+            direction="row"
+            wrap="wrap"
 
-        const fetchPostCodesData = useDebouncedCallback((term) => {
-            setPostcodeError(false);
-            fetch(`https://api.postcodes.io/postcodes/${term}/autocomplete`)
-                .then((response) => response.json())
-                .then((data) => {
-                    setAc(data.result);
-                });
-        }, 500);
+        >
+            <Autocomplete
+                id="search"
+                label="Search Postcode"
+                placeholder="Start typing..."
+                limit={10}
+                onChange={(term) => handleChange(term)}
+                data={ac}
+                onOptionSubmit={(option) => { handleSearch(option) }}
+                defaultValue={searchParams.get('query')?.toString()}
+                error={postcodeError? 'Invalid Postcode' : false}
 
-        return (
-            <Flex
-                mih={50}
-                gap="xl"
-                justify="center"
-                align="flex-end"
-                direction="row"
-                wrap="wrap"
+            />
 
+            <Button
+                autoContrast
+                variant="gradient"
+                gradient={{ from: 'blue', to: 'cyan', deg: 90 }}
+                size='sm'
+                radius={5}
+                mt={10}
+                onClick={() => { handleSearch(postcodeInput) }}
             >
-                <Autocomplete
-                    id="search"
-                    label="Search Postcode"
-                    placeholder="Start typing..."
-                    limit={5}
-                    onChange={(term) => fetchPostCodesData(term)}
-                    data={ac}
-                    onOptionSubmit={(option) => { handleSearch(option) }}
-                    defaultValue={searchParams.get('query')?.toString()}
-                    error={postcodeError}
-
-                />
-
-                <Button
-                    autoContrast
-                    variant="gradient"
-                    gradient={{ from: 'blue', to: 'cyan', deg: 90 }}
-                    size='sm'
-                    radius={5}
-                    mt={10}
-                    onClick={() => { handleSearch('') }}
-                >
-                    Submit
-                </Button>
-            </Flex>
+                Submit
+            </Button>
+        </Flex>
 
 
-        );
-    }
+    );
+}
